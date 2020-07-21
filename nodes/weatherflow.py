059a01fb01cda6c6d4a3cafacb59ea2acb61cff6
Bob Paauwe <bpaauwe@yahoo.com>
2020-07-21 10:55:33 -0700

    Add some error trapping around monthly and weekly rain.
    New stations may not have any data in the database yet.

--- nodes/weatherflow.py
+++ nodes/weatherflow.py
@@ -245,39 +245,44 @@ class Controller(polyinterface.Controller):
             today = datetime.datetime.today()
             y_rain = 0
             for month in range(1,today.month+1):
-                m_rain = 0
-                # get epoch time for start of month and end of month
-                datem = datetime.datetime(today.year, month, 1)
-                start_date = datem.replace(day=1)
-                #end_date = datem.replace(month=(month+1 % 12), day=1) - datetime.timedelta(days=1)
-                end_date = datem.replace(month=(month+1 % 12), day=1)
+                try:
+                    m_rain = 0
+                    # get epoch time for start of month and end of month
+                    datem = datetime.datetime(today.year, month, 1)
+                    start_date = datem.replace(day=1)
+                    #end_date = datem.replace(month=(month+1 % 12), day=1) - datetime.timedelta(days=1)
+                    end_date = datem.replace(month=(month+1 % 12), day=1)
 
-                # make request:
-                #  /swd/rest/observations/device/<id>?time_start=start&time_end=end&api_key=
-                path_str = '/swd/rest/observations/device/'
-                path_str += str(device_id) + '?'
-                path_str += 'time_start=' + str(int(start_date.timestamp()))
-                path_str += '&time_end=' + str(int(end_date.timestamp()))
-                path_str += '&api_key=6c8c96f9-e561-43dd-b173-5198d8797e0a'
+                    # make request:
+                    #  /swd/rest/observations/device/<id>?time_start=start&time_end=end&api_key=
+                    path_str = '/swd/rest/observations/device/'
+                    path_str += str(device_id) + '?'
+                    path_str += 'time_start=' + str(int(start_date.timestamp()))
+                    path_str += '&time_end=' + str(int(end_date.timestamp()))
+                    path_str += '&api_key=6c8c96f9-e561-43dd-b173-5198d8797e0a'
 
-                LOGGER.info('path = ' + path_str)
+                    LOGGER.info('path = ' + path_str)
 
-                c = http.request('GET', path_str)
-                awdata = json.loads(c.data.decode('utf-8'))
+                    c = http.request('GET', path_str)
+                    awdata = json.loads(c.data.decode('utf-8'))
 
-                # we should now have an array of observations
-                for obs in awdata['obs']:
-                    # for sky, index 3 is daily rain.  for tempest it is index 12
-                    if sky_found:
-                        m_rain += obs[3]
-                        y_rain += obs[3]
-                    elif tempest_found:
-                        m_rain += obs[12]
-                        y_rain += obs[12]
+                    # we should now have an array of observations
+                    for obs in awdata['obs']:
+                        # for sky, index 3 is daily rain.  for tempest it is index 12
+                        if sky_found:
+                            m_rain += obs[3]
+                            y_rain += obs[3]
+                        elif tempest_found:
+                            m_rain += obs[12]
+                            y_rain += obs[12]
 
-                LOGGER.info('Month ' + str(month) + ' had rain = ' + str(m_rain))
+                    LOGGER.info('Month ' + str(month) + ' had rain = ' + str(m_rain))
 
-                c.close()
+                    c.close()
+                except:
+                    LOGGER.error('Failed to get rain for month %d' % month);
+                    c.close()
+
             LOGGER.info('yearly rain total = ' + str(y_rain))
 
             # Need to do a separate query for weekly rain
@@ -291,16 +296,21 @@ class Controller(polyinterface.Controller):
 
             LOGGER.info('path = ' + path_str)
 
-            c = http.request('GET', path_str)
-            awdata = json.loads(c.data.decode('utf-8'))
-            w_rain = 0
-            for obs in awdata['obs']:
-                if sky_found:
-                    w_rain += obs[3]
-                elif tempest_found:
-                    w_rain += obs[12]
+            try:
+                c = http.request('GET', path_str)
+                awdata = json.loads(c.data.decode('utf-8'))
+                w_rain = 0
+                for obs in awdata['obs']:
+                    if sky_found:
+                        w_rain += obs[3]
+                    elif tempest_found:
+                        w_rain += obs[12]
 
-            c.close()
+                c.close()
+            except:
+                LOGGER.error('Failed to get weekly rain')
+                c.close()
+
             LOGGER.info('weekly rain total = ' + str(w_rain))
 
             http.close()
