c4479b3e58e58a138c334e20f3e6b636e8bed94d
Bob Paauwe <bpaauwe@yahoo.com>
2021-12-17 16:40:01 -0800

    Add SO_REUSEPORT socket option.
    Allow multiple applications to use the same address/port
    combination

--- nodes/weatherflow.py
+++ nodes/weatherflow.py
@@ -766,6 +766,7 @@ class Controller(polyinterface.Controller):
     def udp_data(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         s.bind(('0.0.0.0', self.params.get('ListenPort')))
         self.windspeed = 0
         sky_tm = 0
